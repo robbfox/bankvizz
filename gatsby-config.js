185100f16c9e04bbe106564c8df277d5ab4d9c03
robbfox <robbfox@outlook.com>
2025-06-20 10:20:03 +0100

    gatsby-config fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 export default  {
   siteMetadata: {
     title: `Robb Fox - banking app`,
